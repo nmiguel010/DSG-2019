6d7ee2b026337b1c7309b176dbeb50074f7d2c5e
Nagore Miguel Pavon <nagomiguel@hotmail.com>
2019-10-02 17:08:56 +0200

    Test

---
+++ TestPlayer/TestPlayer.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../TextGame/Player.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestPlayer
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+
+	};
+}
---
+++ TestWorld/TestWorld.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../TextGame/World.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestWorld
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
